0701f47da0ba252682858526b51fecaf2383ba90
carlkiptoo <carlosiamcarl@gmail.com>
2025-04-26 15:15:00 +0300

    program creation route

---
+++ routes/programRoutes.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import {createProgram} from '../controllers/programController.js';
+
+
+const router = express.Router();
+
+//This creates a new program
+router.post('/program', createProgram);
+
+export default router;
